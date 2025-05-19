f75d2b38b9d0e723e459f1c970ccea029148e8c6
Rodrigo J. S. Freitas <rjesusdasilvafreitas@gmail.com>
2025-05-19 17:06:06 -0300

    correção de bugs e refatoração do Package

--- app/src/main/java/com/nitro/arquivomobileoficialnitro/ui/theme/Font.kt
+++ app/src/main/java/com/nitro/arquivomobileoficialnitro/ui/theme/Font.kt
@@ -1,8 +1,8 @@
-package com.example.myapplication.ui.theme
+package com.nitro.arquivomobileoficialnitro.ui.theme
 
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import com.example.arquivomobileoficialnitro.R
+import com.nitro.arquivomobileoficialnitro.R
 
 val archivoBlack = FontFamily(Font(R.font.archivo_black))
 val archivo= FontFamily(Font(R.font.archivo))
